1a9cf39b1b6a798f887ff2d35666cc73e3650dfa
aishadhin <aishadhin.ac@gmail.comm>
2022-04-30 06:47:34 -0400

    banner added

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -2,16 +2,11 @@ import React from 'react';
 import Background from '../../../img/banner.jpg'
 import './Banner.css'
 
-
-var sectionStyle = {
-    width: "100%",
-    height: "400px",
-    backgroundImage: "url(" + { Background } + ")"
-  };
 const Banner = () => {
     return (
-        <div style={ sectionStyle } className='container-fluid'>
-
+        <div style={{backgroundImage:`url(${Background})`, backgroundSize:'cover', padding:'200px 0'}} className='container-fluid text-center text-white'>
+            <h1 className='my-auto'>Shadhin Photography</h1>
+            <p className='my-auto'>Capture the moment</p>
         </div>
     );
 };
