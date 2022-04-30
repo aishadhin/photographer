65b436f3e08e4c92ac99e38006213d44736f39b0
aishadhin <aishadhin.ac@gmail.comm>
2022-04-30 07:18:44 -0400

    checkout create

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
 import Blogs from './Components/Blogs/Blogs';
+import CheckOut from './Components/CheckOut/CheckOut';
 import Home from './Components/Home/Home/Home';
 import Services from './Components/Home/Services/Services';
 import Login from './Components/Login/Login';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/services' element={<Services></Services>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
---
+++ src/Components/CheckOut/CheckOut.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import che from '../../img/checkout.jpg'
+
+const CheckOut = () => {
+    return (
+        <div className='container text-center'>
+            <h2>Welcome to CheckOut page</h2>
+            <img src={che} alt=''/>
+        </div>
+    );
+};
+
+export default CheckOut;
