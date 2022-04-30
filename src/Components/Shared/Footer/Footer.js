1477096cd0fbeeb235f2639220ceecdf68b1e801
aishadhin <aishadhin.ac@gmail.comm>
2022-04-30 03:29:13 -0400

    header and footer added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='my-5 text-center text-primary'>&#169;All right reserve to Shadhin photography</p>
+            <p className='my-5 text-center text-primary'>&#169; All right reserve to Shadhin photography</p>
         </div>
     );
 };
